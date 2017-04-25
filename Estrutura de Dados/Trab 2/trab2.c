83766717e14f6d8dda593d52a4ccc61e5c62101a
Matheus <Matheus>
2017-04-25 20:38:57 -0300

    Básico do trabalho 2 de Estrutura de Dados

---
+++ Estrutura de Dados/Trab 2/trab2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define TAM 100;
+
+int main(int argc, char **argv)
+{
+	int n,m;
+	int i,j;
+	int a = a2 = 0;
+	
+	scanf("%i %i",n,m);
+	
+	int c, c2, c3;
+	do{
+		int v[n];
+	for(c = 0; c < n; c++){
+		v[c] = c+1;
+	}
+	for(c = 0; c < m; c++){
+		scanf("%i %i",i,j);
+		for(c2 = 0; a != i; c2++){
+			a = v[c2];
+		}
+		for(c3 = 0; a2 != j, c3++){
+			a2 = v[c3];
+		}
+		
+		
+	}
+	
+		scanf("%i %i",n,m);
+	}while(n != 0 && m != 0);
+	
+	return 0;
+}
+
